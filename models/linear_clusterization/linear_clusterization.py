26dbfe78d654598f5a2e71f568bd2d1e48837092
Michał Witas <michasiek098@gmail.com>
2023-02-19 19:33:51 +0100

    Typo correction.

--- models/linear_clusterization/linear_clusterization.py
+++ models/linear_clusterization/linear_clusterization.py
@@ -15,22 +15,22 @@ class LinearClusterization:
         A list that stores BoundingBox objects.
 
     angle_step: int
-        An integer number specifying the incrementation. Must be greater 
-        than or equal to 1 and less than or equal to 180.
+        A value by which the current angle changes in compare to the 
+        previous one. Must be greater than or equal to 1 and less than 
+        or equal to 180.
 
     seed: int
-        An integer number. Allows repetition of results.
+        Allows to reproduce a result.
 
     areas: list
-        A list that stores a BoundingBox objects that belong to 
-        specified clusters.
+        A list that stores sublists with BoundingBox objects. Those
+        sublists determine different clusters.
 
 
     Methods:
     -------
     fit(factor)
-        Based on BoundingBox centers the function determines the
-        clusters.
+        Runs training and determines areas (clusters).
     """
 
     def __init__(self, list_of_bounding_boxes: list, angle_step: int,
@@ -119,36 +119,55 @@ class LinearClusterization:
 
     def __sort_distances(self, line):
         """
-        Sorts and returns the distances of points from a straight line 
-        in ascending order.
+        Sorts distances from points to the line in ascending order.
+
+        Attributes:
+        ----------
+        line: LinearFunction
+            An instance of the LinearFunction class with its requirements.
         """
+
+        if not isinstance(line, LinearFunction):
+            raise TypeError(
+                "The line parameter must be of type LinearFunction, not "
+                f"{type(line).__name__}.")
 
         return sorted(line.list_of_distances,
                       key=lambda x: x.point_to_line_distance)
 
-    def __only_distances(self, line):
+    def __only_distances(self, list_of_distances):
         """
-        A list that stores distances from points to the line. This 
-        simplifies the calculations.
+        A list that stores only distances from points to the line, 
+        without indexes.
         """
 
+        if not isinstance(list_of_distances, list):
+            raise TypeError(
+                "The list_of_distances parameter must be of type list, not "
+                f"{type(list_of_distances).__name__}.")
+
+        for element in list_of_distances:
+            if not isinstance(element, tuple):
+                raise TypeError(
+                    "Elements inside the list_of_distances must be of type "
+                    f"tuple, not {type(list_of_distances).__name__}.")
+
         return [distance.point_to_line_distance
-                for distance in line]
+                for distance in list_of_distances]
 
     def __determine_areas(self, factor):
         """
-        Allows to determine the clusters.
+        For each line from list_of_lines_different_angles this function
+        determines different areas.
 
         Parameters:
         ----------
-        factor: 
-            A mathematical function to determine by how much the next 
-            distance of a point from a straight line can be greater 
-            than the current one.
+        factor: Factor 
+            A name of method from the Factor class.
 
         Returns:
         -------
-        A list with determined clusters.
+        A list with determined areas.
         """
 
         areas = []
@@ -165,7 +184,7 @@ class LinearClusterization:
 
                 case _:
                     raise AttributeError(
-                        """The specified coefficient name does not exist, 
+                        """The specified factor name does not exist, 
                         select one of the coefficients from the list:\n
                             max_mean
                         """)
@@ -203,12 +222,12 @@ class LinearClusterization:
 
     def __areas_length(self, areas):
         """
-        Returns a list with the length of areas.
+        Returns a list that stores number of elements in each area.
 
         Parameters:
         ----------
         areas:
-            Clusters.
+            Determined clusters.
         """
 
         return [
@@ -217,27 +236,29 @@ class LinearClusterization:
 
     def __max_area_length(self, areas_length):
         """
-        Returns a maximum length of the area.
+        Returns a value that determiens the maximum number of elements 
+        in the area.
 
         Parameters:
         ----------
         areas_length:
-            A list with the cluster's length.
+            A list that stores number of elements in each area.
         """
 
         return max(areas_length)
 
     def __get_areas_max_length_index(self, areas_length, max_area_length):
         """
-        Returns an index(es) of the longest areas.
+        Returns index(es) of the area(s) with the maximum number of 
+        elements.
 
         Parameters:
         ----------
-        areas_length:
-            A list with the cluster's length.
+        areas_length: list
+            A list that stores number of elements in each area.
 
         max_area_length: int
-            Maximum cluster's length.
+            Maximum number of elements
         """
 
         return np.where(
@@ -245,13 +266,14 @@ class LinearClusterization:
 
     def __find_minimum_distances(self, areas_index_max_length, det_areas):
         """
-        In case of the multiple areas with the same length, this method
-        calculates the total distance of points to the line.
+        In case of the multiple areas with the same number of elements, 
+        this method calculates the total distance from points to the 
+        line.
 
         Parameters:
         ----------
         areas_index_max_length:
-            An list that contains indexes with maximum number of 
+            A list that contains index(es) with the maximum number of 
             elements.
 
         det_areas:
@@ -274,13 +296,13 @@ class LinearClusterization:
 
     def __find_best_area(self, areas):
         """
-        Returns an index with the best area.
+        Returns the list position of the area with the shortest distance
+        from points to the line.
 
         Parameters:
         ----------
         areas:
-            A list that contains the sums of distances of points from 
-            a straight line.
+            A list with total distances from points to the line.
         """
 
         return areas.index(min(areas))
@@ -291,25 +313,27 @@ class LinearClusterization:
 
         Parameters:
         ----------
-        factor:
-            A method from the Factor class.
+        factor: str
+            A name of the method from the Factor class.
         """
 
         while len(self.list_of_bounding_boxes) != 0:
-            # determining areas for each line
+            # determining areas for the each line
             determined_areas = self.__determine_areas(factor)
-            # calculating the area length for each line
+            # determining a number of elements in the area for the
+            # each line
             areas_length = self.__areas_length(determined_areas)
-            # getting the area's maximum length
+            # determining maximum length of the area
             max_area_length = self.__max_area_length(areas_length)
-            # determining the index(es) of the longest areas
+            # determining index(es) of areas with the largest number
+            # of elements
             area_max_length_index = self.__get_areas_max_length_index(
                 areas_length, max_area_length)
-            # determining the list of sum of distances from points
-            # to line
+            # for each area with the largest number of elements calculate
+            # the total distance from these points to the line
             area_min_distance = self.__find_minimum_distances(
                 area_max_length_index, determined_areas)
-            # determining the index of the shortest distance
+            # determining the index of area with the least distance
             shortest_area_distance_index = self.__find_best_area(
                 area_min_distance)
 
