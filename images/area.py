0476611839ae99a4ef9fc1b380101b88acbe3f56
Michal <michasiek098@gmail.com>
2023-03-03 20:18:55 +0100

    Functionality development

--- images/area.py
+++ images/area.py
@@ -6,10 +6,26 @@ from images.bounding_box import BoundingBox
 
 
 class ParkingArea:
+    """
+    Stores informations about a parking area: its endpoints, number of
+    occupied parking spots and total number of parking spots. Also
+    allows you to draw the area and check if an object is inside the
+    area.
+
+    Methods:
+    -------
+    is_in_area
+    draw_area
+    forget_detected_cars
+    """
+
+    __color = [255, 255, 0]
+
     def __init__(self, area: list, img_width: int, img_height: int):
         self.area = area
         self.img_width = img_width
         self.img_height = img_height
+        self.occupied_parking_spots = 0
 
     @property
     def area(self):
@@ -29,6 +45,10 @@ class ParkingArea:
                     f"BoundingBox, not {type(bbox).__name__}.")
 
         self._area = value
+
+    @property
+    def number_of_parking_spots(self):
+        return len(self.area) - 1
 
     @property
     def img_width(self):
@@ -56,6 +76,10 @@ class ParkingArea:
 
         self._img_height = value
 
+    @property
+    def __color(self):
+        return np.random.randint(0, 255, size=(1, 3)).tolist()[0]
+
     def __calculate_angles(self):
         return [
             int(
@@ -77,13 +101,10 @@ class ParkingArea:
                 key=lambda pair: pair[0])
         ]
 
-    # def abcdef(self):
-    #   print(self.__sort_bboxes_by_angle())
     @property
     def top_left_coordinate(self):
         return Coordinate(x=self.__sort_bboxes_by_angle()[-1].top_left_x,
-                          y=(self.__sort_bboxes_by_angle()[-1].top_left_y
-                          + self.__sort_bboxes_by_angle()[-1].bbox_height))
+                          y=(self.__sort_bboxes_by_angle()[-1].top_left_y))
 
     @property
     def top_right_coordinate(self):
@@ -95,54 +116,85 @@ class ParkingArea:
     def bottom_left_coordinate(self):
         return Coordinate(x=self.__sort_bboxes_by_angle()[0].top_left_x,
                           y=(self.__sort_bboxes_by_angle()[0].top_left_y
-                          + self.__sort_bboxes_by_angle()[0].bbox_height))
+                             + self.__sort_bboxes_by_angle()[0].bbox_height))
 
-    @ property
+    @property
     def bottom_right_coordinate(self):
         return Coordinate(x=(self.__sort_bboxes_by_angle()[0].top_left_x
                              + self.__sort_bboxes_by_angle()[0].bbox_width),
                           y=(self.__sort_bboxes_by_angle()[0].top_left_y
                              + self.__sort_bboxes_by_angle()[0].bbox_height))
 
+    def __numeric_area(self):
+        return np.array([
+            [self.top_left_coordinate.x, self.top_left_coordinate.y],
+            [self.top_right_coordinate.x, self.top_right_coordinate.y],
+            [self.bottom_right_coordinate.x, self.bottom_right_coordinate.y],
+            [self.bottom_left_coordinate.x, self.bottom_left_coordinate.y]
+        ])
+
     def is_in_area(self, point: Coordinate):
+        """
+
+        """
         if not isinstance(point, Coordinate):
             raise TypeError(
                 "The point parameter must be of type Coordinate, "
                 f"not {type(point).__name__}.")
 
-            area = (
-                (self.top_left_coordinate.x, self.top_left_coordinate.y),
-                (self.top_right_coordinate.x, self.top_right_coordinate.y),
-                (self.bottom_right_coordinate.x, self.bottom_right_coordinate.y),
-                (self.bottom_left_coordinate.x, self.bottom_left_coordinate.y)
-            )
+        return cv2.pointPolygonTest(
+            contour=self.__numeric_area(),
+            pt=(point.x, point.y),
+            measureDist=False
+        )
 
-            return cv2.pointPolygonTest(
-                contour=area,
-                pt=(point.x, point.y),
-                measureDist=False
-            )
+    @property
+    def occupied_parking_spots(self):
+        return self._occupied_parking_spots
+
+    @occupied_parking_spots.setter
+    def occupied_parking_spots(self, value):
+        if not isinstance(value, int):
+            raise TypeError(
+                "The occupied_parking_spots must be of type int, "
+                f"not {type(value).__name__}.")
+
+        self._occupied_parking_spots = value
+
+    def forget_detected_cars(self):
+        """
+
+        """
+        self.occupied_parking_spots = 0
+
+    # def prepare_img(self, img):
+    #     img[self.top_left_coordinate.y:self.top_left_coordinate.y - 30,
+    #         self.top_left_coordinate.x:self.top_right_coordinate.x
+    #         ] = 0
+    #     return img
 
     def draw_area(self, img: np.ndarray):
-        abcdef = np.array(
+        """
 
-            [
-                [int(self.top_left_coordinate.x),
-                 int(self.top_left_coordinate.y)],
-                [int(self.top_right_coordinate.x),
-                 int(self.top_right_coordinate.y)],
-                [int(self.bottom_right_coordinate.x),
-                 int(self.bottom_right_coordinate.y)],
-                [int(self.bottom_left_coordinate.x),
-                 int(self.bottom_left_coordinate.y)]
-            ]
-
-        )
+        """
 
         return cv2.polylines(
             img=img,
-            pts=[abcdef],
+            pts=[self.__numeric_area()],
             isClosed=True,
-            color=np.random.randint(0, 255, size=(1, 3)).tolist()[0],
-            thickness=3
-        )
+            color=[255, 100, 100],
+            thickness=3)
+
+    def print_information(self, img: np.ndarray):
+        """
+
+        """
+        return cv2.putText(
+            img=img,
+            text=(f"{self.occupied_parking_spots} car(s) in "
+                  f"{self.number_of_parking_spots} spots."),
+            org=(self.top_left_coordinate.x, self.top_left_coordinate.y-10),
+            fontFace=cv2.FONT_HERSHEY_SIMPLEX,
+            fontScale=0.5,
+            color=[255, 100, 100],
+            thickness=2)
--- run_project.py
+++ run_project.py
@@ -10,11 +10,12 @@ from images.coordinate import Coordinate
 from images.area import ParkingArea
 
 video = cv2.VideoCapture(
-    "/home/misiek/Pulpit/python/videoplayback.mp4")
+    "/home/misiek/Pulpit/python/cars_on_parking.mp4")
 
 # lines_yolo_model = YoloModel("path_to_model")
-# cars_yolo_model = YoloModel("path_to_model")
-# cars_yolo_model.model.classes = [2]
+cars_yolo_model = YoloModel(
+    "/home/misiek/Pulpit/python/yolov5/yolov5s.pt")
+cars_yolo_model.model.classes = [2]
 
 # frame_number = 0
 # # 1. Wykryj linie
@@ -23,16 +24,6 @@ video = cv2.VideoCapture(
 # # 4. Wykryj pojazdy
 # # 5. Sprawdź czy pojazdy są wewnątrz strefy
 
-coords = "680 761 255 99 586 689 286 98 550 623 268 87 521 567 253 77 494 516 240 71 471 473 228 63 451 437 218 55 432 397 207 54 413 366 200 47 398 336 192 45 383 309 185 41 1255 660 272 124 1173 600 255 108 1104 547 238 95 1042 499 223 83 985 452 215 77 936 413 205 69 891 378 200 63 851 346 191 58 817 317 181 55 785 292 170 49 755 267 167 47 729 244 160 43 704 222 154 42 680 204 149 41 658 188 143 36 638 171 139 36 620 153 140 36 1432 415 169 58 1361 379 163 52 1298 345 162 51 1243 315 155 44 1186 287 154 41 1141 262 144 39 1097 239 140 38 1056 216 138 38 1020 196 133 37 987 179 125 32 954 162 124 31 923 144 123 31 895 132 118 28 869 117 116 27"
-coords = np.array([int(e) for e in coords.split(" ")]).reshape(-1, 4)
-
-list_of_parking_lines = []
-
-for x, y, w, h in coords:
-    list_of_parking_lines.append(
-        BoundingBox([Coordinate(int(x), int(y)),
-                     Coordinate(int(x+w), int(y+h))])
-    )
 
 frame_number = 0
 
@@ -41,14 +32,25 @@ while video.isOpened():
     if not ret:
         break
 
-    # img = frame.copy()
-    img = cv2.imread("/home/misiek/Pulpit/python/parking_spots/data/file.bmp")
+    img = frame.copy()
 
     if frame_number == 0:
         # print("Detecting parking lines...")
         # list_of_parking_lines = lines_yolo_model.detect(img)
         # print(f"Found {len(list_of_parking_lines)} parking lines.")
         # print()
+
+        coords = "680 761 255 99 586 689 286 98 550 623 268 87 521 567 253 77 494 516 240 71 471 473 228 63 451 437 218 55 432 397 207 54 413 366 200 47 398 336 192 45 383 309 185 41 1255 660 272 124 1173 600 255 108 1104 547 238 95 1042 499 223 83 985 452 215 77 936 413 205 69 891 378 200 63 851 346 191 58 817 317 181 55 785 292 170 49 755 267 167 47 729 244 160 43 704 222 154 42 680 204 149 41 658 188 143 36 638 171 139 36 620 153 140 36 1432 415 169 58 1361 379 163 52 1298 345 162 51 1243 315 155 44 1186 287 154 41 1141 262 144 39 1097 239 140 38 1056 216 138 38 1020 196 133 37 987 179 125 32 954 162 124 31 923 144 123 31 895 132 118 28 869 117 116 27"
+        coords = np.array([int(e) for e in coords.split(" ")]).reshape(-1, 4)
+
+        list_of_parking_lines = []
+
+        for x, y, w, h in coords:
+            list_of_parking_lines.append(
+                BoundingBox([Coordinate(int(x), int(y)),
+                            Coordinate(int(x+w), int(y+h))])
+            )
+
         print("Looking for areas...")
         linear_cluster = LinearClusterization(
             list_of_bounding_boxes=list_of_parking_lines,
@@ -61,14 +63,34 @@ while video.isOpened():
             for area in linear_cluster.areas
         ]
 
-        for a in list_of_areas:
-            a.draw_area(img)
+    elif frame_number % 30 == 0:
+        detected_cars = cars_yolo_model.detect(frame)
+        print(f"Found {len(detected_cars)} cars.")
 
-        cv2.imshow("abcdef", img)
-        cv2.waitKey(0)
+        for car in detected_cars:
+            car_bbox_center = Coordinate(car.x_center, car.y_center)
 
-    else:
-        break
+            for area in list_of_areas:
+                if area.is_in_area(car_bbox_center) == 1:
+                    area.occupied_parking_spots += 1
 
-#     frame_number += 1
-#     # else:
+        for area in list_of_areas:
+            area.prepare_img(img)
+            area.draw_area(img)
+            area.print_information(img)
+
+        cv2.imshow("img", img)
+        cv2.waitKey(1)
+
+    for area in list_of_areas:
+        print(
+            f"{area.occupied_parking_spots} cars in "
+            f"{area.number_of_parking_spots} spots")
+    print()
+    print()
+
+    for area in list_of_areas:
+        # area.occupied_parking_spots = 0
+        area.forget_detected_cars()
+
+    frame_number += 1
